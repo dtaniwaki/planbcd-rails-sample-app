121ce9eaf46acd7659f04c00ecd71eec16ceb5c0
dtaniwaki <daisuketaniwaki@gmail.com>
2014-05-28 22:51:37 +0900

    Update planbcd-rails 0.1.1

--- config/initializers/planbcd.rb
+++ config/initializers/planbcd.rb
@@ -1,7 +1,7 @@
 PlanBCD::Rails.configure do |c|
   # Set your JS file found in your offer.
-  c.js = "//localhost/abc/def.js"
-  # c.js = proc { |controller| controller.is_something? ? "//localhost/abc/def_1.js" : "//localhost/abc/def_2.js" }
+  c.js_url = "//localhost/abc/def.js"
+  # c.js_url = proc { |controller| controller.is_something? ? "//localhost/abc/def_1.js" : "//localhost/abc/def_2.js" }
 
   # Insert PlanBCD js code to any response for html request
   c.auto_insert = true
