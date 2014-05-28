90cd8689b463153c14d00992394954a33365ff25
dtaniwaki <daisuketaniwaki@gmail.com>
2014-05-28 19:50:50 +0900

    Use auto_insert

--- Gemfile
+++ Gemfile
@@ -39,3 +39,4 @@ gem 'spring',        group: :development
 # gem 'debugger', group: [:development, :test]
 
 gem 'planbcd-rails'
+#gem 'planbcd-rails', path: '../planbcd-rails'
--- config/initializers/planbcd.rb
+++ config/initializers/planbcd.rb
@@ -2,4 +2,7 @@ PlanBCD::Rails.configure do |c|
   # Set your JS file found in your offer.
   c.js = "//localhost/abc/def.js"
   # c.js = proc { |controller| controller.is_something? ? "//localhost/abc/def_1.js" : "//localhost/abc/def_2.js" }
+
+  # Insert PlanBCD js code to any response for html request
+  c.auto_insert = true
 end
